212cca3603e6a8e397600881598efd7520c48a3f
Erich Styger <erich.styger@hslu.ch>
2026-08-12 16:54:11 +0200

    merged application file

--- src/application.c
+++ src/application.c
@@ -72,22 +72,44 @@ void Application_StateStopCalibrate(void) {
 
 #if PL_CONFIG_USE_DEBOUNCE
 void Application_OnButtonEvent(Buttons_e button, McuDbnc_EventKinds event) { /* called from debouncing */
-  //McuLog_info("on debounce event: button %d, event %d", button, event);
+#if PL_CONFIG_IS_ESP32 && PL_CONFIG_USE_REMOTE_NORDIC
+  RemoteNordic_ESP32OnButtonEvent(button, event);
+#endif
+#if PL_CONFIG_IS_ROBOT
   if (button==BUTTONS_USER) {
     if (event==MCUDBNC_EVENT_RELEASED) {
       #if PL_CONFIG_APP_LINE_FOLLOWING
         LineFollow_StartStopFollowing(); /* button press toggles line following mode */   
         #if PL_CONFIG_USE_BUZZER 
-        Buzzer_PlayTune(BUZZER_TUNE_BUTTON);
+          Buzzer_PlayTune(BUZZER_TUNE_BUTTON);
         #endif
       #endif
     }
   }
+#endif /* PL_CONFIG_IS_ROBOT */
 }
 #endif /* PL_CONFIG_USE_DEBOUNCE */
 
-#if PL_CONFIG_USE_APP_TASK
+#if PL_CONFIG_IS_ESP32
+static void AppTask(void *pv) {
+  for(;;) {
+#if PL_CONFIG_USE_BUTTONS && !PL_CONFIG_USE_BUTTONS_IRQ
+    /*! \TODO if enabled WiFi, it triggers GPIO button interrupts? Doing polling instead */
+    uint32_t buttons;
 
+    buttons = Buttons_GetButtons();
+    if (buttons!=0) { /* poll buttons */
+      Debounce_StartDebounce(buttons);
+    }
+    vTaskDelay(pdMS_TO_TICKS(20));
+#else
+    vTaskDelay(pdMS_TO_TICKS(1000));
+#endif
+  }
+}
+#endif /* PL_CONFIG_IS_ESP32 */
+
+#if PL_CONFIG_IS_ROBOT
 static AppStateType StateMachine(bool buttonPress, AppStateType currState) {
   switch (currState) {
     case APP_STATE_STARTUP:
@@ -103,7 +125,9 @@ static AppStateType StateMachine(bool buttonPress, AppStateType currState) {
   } /* switch */
   return currState;
 }
+#endif /* PL_CONFIG_IS_ROBOT */
 
+#if PL_CONFIG_IS_ROBOT
 static void AppTask(void *pv) {
 #if PL_CONFIG_USE_I2C && MCU_LIS2DH_CONFIG_IS_ENABLED
   if (McuLis2dh_Init()!=ERR_OK) {
@@ -117,7 +141,76 @@ static void AppTask(void *pv) {
     vTaskDelay(pdMS_TO_TICKS(100));
   }
 }
+#endif /* PL_CONFIG_IS_ROBOT */
+
+#if !PL_CONFIG_USE_MQTT_CLIENT
+void Application_MqttTaskResume(void) {
+  /* dummy, do nothing if called by WiFi task */
+}
+
+void Application_MqttTaskSuspend(void) {
+  /* dummy, do nothing if called by WiFi task */
+}
+#else /* PL_CONFIG_USE_MQTT_CLIENT */
+static TaskHandle_t mqttTaskHandle = NULL;
+
+void Application_MqttTaskResume(void) {
+  if (mqttTaskHandle!=NULL) {
+    vTaskResume(mqttTaskHandle);
+  }
+}
+
+void Application_MqttTaskSuspend(void) {
+  if (mqttTaskHandle!=NULL) {
+    vTaskSuspend(mqttTaskHandle);
+  }
+}
+
+static void MqttTask(void *pv) {
+#if PL_CONFIG_USE_MQTT_SENSOR || PL_CONFIG_USE_MQTT_GAME
+  bool firstTime = true;
+  bool isEnabled = false;
 #endif
+for(;;) {
+#if PL_CONFIG_USE_MQTT_SENSOR
+    if (McuMqttClient_CanPublish()) {
+      if (firstTime) {
+        firstTime = false;
+        isEnabled = MqttSensor_GetIsEnabled();
+        MqttSensor_Publish_Enabled(isEnabled);
+      }
+      if (MqttSensor_GetIsEnabled()) {
+        float t, h;
+        h = Sensirion_GetHumidity();
+        t = Sensirion_GetTemperature();
+        if (MqttSensor_Publish_SensorValues(t, h)!=ERR_OK) {
+          McuLog_error("failed publishing sensor values");
+        }
+      }
+    }
+#elif PL_CONFIG_USE_MQTT_GAME
+    if (MqttClient_CanPublish()) {
+      McuLog_info("query robot battery status");
+      Game_QueryRobotBatteryVoltage(); /* periodically query robot battery voltage */
+      if (firstTime) {
+        firstTime = false;
+        isEnabled = MqttGame_GetIsEnabled();
+        MqttGame_Publish_Enabled(isEnabled);
+      }
+      if (MqttGame_GetIsEnabled()) {
+        if (MqttGame_Publish_RobotStatus()!=ERR_OK) {
+          McuLog_error("failed publishing robot status");
+        }
+        if (MqttGame_Publish_EspStatus()!=ERR_OK) {
+          McuLog_error("failed publishing ESP status");
+        }
+      }
+    }
+#endif
+    vTaskDelay(pdMS_TO_TICKS(5000));
+  } /* for */
+}
+#endif /* PL_CONFIG_USE_MQTT_CLIENT */
 
 void Application_Init(void) {
 #if PL_CONFIG_USE_APP_TASK
@@ -125,6 +218,20 @@ void Application_Init(void) {
     for(;;){} /* error */
   }
 #endif
+#if PL_CONFIG_USE_MQTT_CLIENT
+  if (xTaskCreate(
+      MqttTask,  /* pointer to the task */
+      "mqtt", /* task name for kernel awareness debugging */
+      4*1024/sizeof(StackType_t), /* task stack size */
+      (void*)NULL, /* optional task startup argument */
+      tskIDLE_PRIORITY+2,  /* initial priority */
+      &mqttTaskHandle /* optional task handle to create */
+    ) != pdPASS)
+  {
+    McuLog_fatal("Failed creating MQTT task");
+    for(;;){} /* error! probably out of memory */
+  }
+#endif /* PL_CONFIG_USE_MQTT_CLIENT */
 }
 
 #if PL_CONFIG_IS_ROBOT
