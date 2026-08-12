c68a461bd78fa91813bf80819fc726bdfa2b74fc
Erich Styger <erich.styger@hslu.ch>
2026-08-12 16:04:16 +0200

    merged files

--- src/application.h
+++ src/application.h
@@ -1,6 +1,5 @@
 /*
- * Copyright 2026, Erich Styger
- * All rights reserved.
+ * Copyright 2023-2026, Erich Styger
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -21,35 +20,31 @@ extern "C" {
 
 #include "platform.h"
 #include <stdbool.h>
+#include <stdint.h>
 
 #if PL_CONFIG_USE_REFLECTANCE
 /*!
  * \brief Determines if we are in calibrating state.
  * \return TRUE if calibrating, FALSE otherwise
  */
-bool APP_StateIsCalibrating(void);
+bool Application_StateIsCalibrating(void);
 
 /*!
  * \brief Starts the calibration sequence
  */
-void APP_StateStartCalibrate(void);
+void Application_StateStartCalibrate(void);
 
 /*!
  * \brief Stops the calibration sequence
  */
-void APP_StateStopCalibrate(void);
+void Application_StateStopCalibrate(void);
 
 #endif /* PL_CONFIG_USE_REFLECTANCE */
-
-/*!
- * \brief Runs the main application loop. Does not return.
- */
-void Application_Run(void);
 
 #if PL_CONFIG_USE_DEBOUNCE
  #include "McuDebounce.h"
  #include "buttons.h"
- #include <stdint.h>
+
  /*!
   * \brief Callback invoked by the debounce module on button events.
   * \param button The button that triggered the event.
@@ -57,6 +52,21 @@ void Application_Run(void);
   */
  void Application_OnButtonEvent(Buttons_e button, McuDbnc_EventKinds event);
 #endif
+
+#if MCU_MQTT_CLIENT_CONFIG_ENABLED
+  void Application_MqttTaskResume(void);
+  void Application_MqttTaskSuspend(void);
+#endif
+
+/*!
+ * \brief Runs the main application loop. Does not return.
+ */
+void Application_Run(void);
+
+/*!
+ * \brief Initializes the application module.
+ */
+void Application_Init(void);
 
 #ifdef __cplusplus
 }  /* extern "C" */
