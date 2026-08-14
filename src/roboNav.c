20b8b1a3c88639ebe6ad8c86ee485b713faa132f
Erich Styger <erich.styger@hslu.ch>
2026-08-14 08:10:48 +0200

    added new module

---
+++ src/roboNav.c
@@ -0,0 +1,11 @@
+/**
+ * \file
+ * \brief Robot navigation with a a nav/joystick (real or virtual).
+ * \author Erich Styger, erich.styger@hslu.ch
+ * \license SPDX-License-Identifier: BSD-3-Clause
+ */
+
+ #include "roboNav.h"
+
+ void RoboNav_Init(void) {
+ }
---
+++ src/roboNav.h
@@ -0,0 +1,13 @@
+/**
+ * \file
+ * \brief Robot navigation with a a nav/joystick (real or virtual).
+ * \author Erich Styger, erich.styger@hslu.ch
+ * \license SPDX-License-Identifier: BSD-3-Clause
+ */
+
+ #ifndef __ROBO_NAV_H__
+ #define __ROBO_NAV_H__
+
+void RoboNav_Init(void);
+
+ #endif /* __ROBO_NAV_H__ */
